8781fab1530834a0302c2414d5b55d03ebf78f08
miyaguni <c17018@std.it-college.ac.jp>
2018-10-10 12:12:12 +0900

    add UnityProject

---
+++ BattleRoyal/Assets/Miyaguni/Scripts/PhotonManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonManager : Photon.MonoBehaviour {
+	PhotonView pView;
+
+	void Start(){
+
+	}
+
+	void Connect(){
+	}
+}
