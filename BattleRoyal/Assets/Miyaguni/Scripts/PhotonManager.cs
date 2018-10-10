095beeae3649600930977834bfd4bf671edef68f
miyaguni <c17018@std.it-college.ac.jp>
2018-10-10 12:27:31 +0900

    edit PhotonManager Script

--- BattleRoyal/Assets/Miyaguni/Scripts/PhotonManager.cs
+++ BattleRoyal/Assets/Miyaguni/Scripts/PhotonManager.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class PhotonManager : Photon.MonoBehaviour {
 	PhotonView pView;
+	[SerializeField]
+	string usingSettingsVersion;
 
 	void Start(){
 
 	}
 
 	void Connect(){
+		PhotonNetwork.ConnectUsingSettings(usingSettingsVersion);
 	}
 }
