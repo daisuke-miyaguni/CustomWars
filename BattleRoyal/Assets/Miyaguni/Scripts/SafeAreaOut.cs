6bf798f868a45bfb872bf07475ecea01905235d4
miyaguni <c17018@std.it-college.ac.jp>
2018-10-30 09:46:19 +0900

    add Areareducation

---
+++ BattleRoyal/Assets/Miyaguni/Scripts/SafeAreaOut.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SafeAreaOut : MonoBehaviour
+{
+	[SerializeField] string areaTagName;
+	
+	void OnColliderStay(Collider other)
+	{
+		if(other == null)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ BattleRoyal/Assets/Miyaguni/Scripts/StageManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : MonoBehaviour
+{
+	SphereCollider sphereCollider;
+	[SerializeField] float reducingSpeed;
+    [SerializeField] float collisionOnTime;
+    [SerializeField] float[] reductionScales;
+	[SerializeField] string destroyObjectTag;
+    // reduction
+	int reductionCount;
+
+
+
+	void Start()
+	{
+		reductionCount = 0;
+		sphereCollider = GetComponent<SphereCollider>();
+		sphereCollider.enabled = false;
+		gameObject.transform.position = new Vector3(Random.Range(-60f, 60f), 0.0f, Random.Range(-60f, 60f));
+		
+		Invoke("TriggerOn", collisionOnTime);
+	}
+
+	void TriggerOn()
+	{
+		sphereCollider.enabled = true;
+	}
+	
+	void Update()
+	{
+		if(sphereCollider.radius > reductionScales[reductionCount])
+		{
+			Reduction();
+		}
+
+		if(Input.GetMouseButtonDown(0)){
+			ReceveReductionEvent();
+		}
+	}
+	void Reduction()
+	{
+		sphereCollider.radius -= reducingSpeed * Time.deltaTime;
+	}
+
+	public void ReceveReductionEvent()
+	{
+		reductionCount++;
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if(other.gameObject.tag == destroyObjectTag)
+		{
+			other.gameObject.SetActive(false);
+		}
+	}
+}
