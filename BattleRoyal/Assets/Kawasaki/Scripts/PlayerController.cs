b706211dc9aebc8d208d74ca292b245b2a3834e0
DaiyaKawasaki_c17006 <c17006@std.it-college.ac.jp>
2018-10-30 11:35:03 +0900

    edit PlayerController

--- BattleRoyal/Assets/Kawasaki/Scripts/PlayerController.cs
+++ BattleRoyal/Assets/Kawasaki/Scripts/PlayerController.cs
@@ -11,13 +11,17 @@ public class PlayerController : MonoBehaviour
     private PhotonView myPV;
     private Rigidbody myRB;
     private Camera myCamera;
-    private Slider hpSlider;
+    [SerializeField] private Slider hpSlider;
 
-    [SerializeField] private GameObject playerUIPrefab;
-    private GameObject playerUIGO;
+    // [SerializeField] private GameObject playerUIPrefab;
     [SerializeField] private float moveSpeed;
-    [SerializeField] public float playerHP;
+    [SerializeField] private float playerHP;
     [SerializeField] private float rotateSpeed;
+
+    int damage = -1;
+    int healing = 2;
+    [SerializeField] private GameObject weaponPrefab;
+    private float weaponPower = 100;
 
     void Start()
     {
@@ -31,130 +35,118 @@ public class PlayerController : MonoBehaviour
             myCamera.transform.parent = transform;
             myCamera.transform.position = transform.position + new Vector3(0, 0.8f, -5);
 
-            playerUIGO = Instantiate(playerUIPrefab);
-            playerUIGO.SendMessage("SetTarget", this, SendMessageOptions.RequireReceiver);
-
-
-
-
-
-            // HPバー
-            // GameObject myUI = Instantiate(playerUIPrefab,transform.position,Quaternion.identity);
+            //HPバー
+            // GameObject myUI = Instantiate(playerUIPrefab, transform.position, Quaternion.identity);
             // myUI.GetComponent<Transform>().SetParent(GameObject.Find("Canvas").GetComponent<Transform>());
-            // hpSlider = myUI.GetComponent<Slider>();
-            // hpSlider.value = playerHP;
+            // hpSlider = myUI.GetComponentInChildren<Slider>();
+            hpSlider.value = playerHP;
         }
 
-    }
-
-    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-    {
-        if (stream.isWriting)
-        {
-            stream.SendNext(playerHP);
-            //stream.SendNext(this.ChatText);
-        }
-        else
-        {
-            playerHP = (int)stream.ReceiveNext();
-            //this.ChatText = (string)stream.ReceiveNext();
-        }
     }
 
     void Update()
+    {
+        if (myPV.isMine)
         {
-            if (myPV.isMine)
+            Move();
+
+            // ChangeHP();
+            // RotateCamera();
+
+            if (playerHP <= 0)
             {
-                // HPManagement();
-                Move();
-                RotateCamera();
-
-                if (playerHP <= 0)
-                {
-                    myPV.RPC("Die", PhotonTargets.All);
-                }
-
+                myPV.RPC("Death", PhotonTargets.All);
             }
 
             if (Input.GetMouseButtonDown(1))
             {
-                myPV.RPC("DecreaseLife", PhotonTargets.All);
+                myPV.RPC("ChangeHP", PhotonTargets.All, damage);
             }
-        }
 
-        private void HPManagement()
-        {
-            hpSlider.transform.position = Camera.main.WorldToScreenPoint(transform.position);
-            hpSlider.transform.position = transform.position;
-            hpSlider.value = playerHP;
-        }
-
-        private void RotateCamera()
-        {
-            // if (Input.GetMouseButton(0))
-            // {
-            Vector3 angle = new Vector3(
-                Input.GetAxis("Mouse X") * rotateSpeed,
-                Input.GetAxis("Mouse Y") * rotateSpeed,
-                0
-            );
-
-            myCamera.transform.RotateAround(transform.position, Vector3.up, angle.x);
-            myCamera.transform.RotateAround(transform.position, myCamera.transform.right, angle.y * -1);
-            // }
-        }
-
-        private void Move()
-        {
-            myRB.velocity = GetMoveDirection() * moveSpeed;
-        }
-
-        private Vector3 GetMoveDirection()
-        {
-            float x = Input.GetAxisRaw("Horizontal");
-            float z = Input.GetAxisRaw("Vertical");
-
-            return new Vector3(x, 0, z);
-        }
-
-        // 回避
-        public void Avoid()
-        {
-        }
-
-        // 攻撃
-        public void Attack()
-        {
-        }
-
-        // 回復
-        public void Recover()
-        {
-        }
-
-
-        public void OnCollisionEnter(Collision other)
-        {
-            // 被弾
-            if (other.gameObject.tag == "weapon")
+            if (Input.GetMouseButtonDown(0))
             {
-                myPV.RPC("DecreaseLife", PhotonTargets.All);
+                myPV.RPC("ChangeHP", PhotonTargets.All, healing);
+            }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                myPV.RPC("Attack", PhotonTargets.All, transform.position, weaponPower);
             }
         }
+    }
+
+    private void Move()
+    {
+        myRB.velocity = GetMoveDirection() * moveSpeed;
+    }
+
+    private void RotateCamera()
+    {
+        // if (Input.GetMouseButton(0))
+        // {
+        Vector3 angle = new Vector3(
+            Input.GetAxis("Mouse X") * rotateSpeed,
+            Input.GetAxis("Mouse Y") * rotateSpeed,
+            0
+        );
+
+        myCamera.transform.RotateAround(transform.position, Vector3.up, angle.x);
+        myCamera.transform.RotateAround(transform.position, myCamera.transform.right, angle.y * -1);
+        // }
+    }
+
+
+    private Vector3 GetMoveDirection()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        return new Vector3(x, 0, z);
+    }
 
     [PunRPC]
-    private void DecreaseLife()
+    private void ChangeHP(int value)
     {
-        playerHP -= 1;
+        playerHP += value;
+        hpSlider.value = playerHP;
     }
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        // 被弾
+        if (myPV.isMine && other.gameObject.tag == "weapon")
+        {
+            myPV.RPC("ChangeHP", PhotonTargets.All, damage);
+        }
+    }
+
+
 
     // 死亡
     [PunRPC]
-    private void Die()
+    private void Death()
     {
-        // Destroy(gameObject);
-        PhotonNetwork.Disconnect();
-        SceneManager.LoadScene("hoge");
+        Destroy(gameObject);
+        // PhotonNetwork.Disconnect();
+        // SceneManager.LoadScene("hoge");
 
+    }
+
+    // 回避
+    public void Avoid()
+    {
+    }
+
+    // 攻撃
+    [PunRPC]
+    private void Attack(Vector3 pos, float power)
+    {
+        GameObject weapon = Instantiate(weaponPrefab, pos, Quaternion.identity);
+        weapon.GetComponent<Rigidbody>().AddForce(Vector3.up * power);
+    }
+
+    // 回復
+    public void Recover()
+    {
     }
 }
