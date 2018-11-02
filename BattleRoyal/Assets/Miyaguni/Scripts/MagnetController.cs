f1a9fd943734ef7d468f1298a3f257ddd93198cc
miyaguni <c17018@std.it-college.ac.jp>
2018-11-02 11:07:26 +0900

    add MagnetController

---
+++ BattleRoyal/Assets/Miyaguni/Scripts/MagnetController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagnetController : MonoBehaviour
+{
+	[SerializeField] float gravityPower;
+    [SerializeField] string playerTag;
+    GameObject player;
+	Rigidbody playerRb;
+	Rigidbody magnetRb;
+
+    void Start()
+	{
+		player = GameObject.FindWithTag(playerTag);
+		playerRb = player.GetComponent<Rigidbody>();
+		magnetRb = GetComponent<Rigidbody>();
+	}
+
+	void OnTriggerStay(Collider other)
+	{
+		if(other.gameObject.tag == playerTag)
+		{
+			Vector3 direction = transform.position - other.transform.position;
+			Vector3 magnetGravity = gravityPower * direction * (magnetRb.mass * playerRb.mass);
+			playerRb.AddForce(magnetGravity);
+		}
+	}
+}
