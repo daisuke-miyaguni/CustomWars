73a67ee6cc8eab64b26f74fa6e660d8d3eec3a77
miyaguni <c17018@std.it-college.ac.jp>
2018-11-07 10:38:17 +0900

    edit tester

--- BattleRoyal/Assets/Miyaguni/Scripts/CollisionTest.cs
+++ BattleRoyal/Assets/Miyaguni/Scripts/CollisionTest.cs
@@ -2,18 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// 動作テストのためのプログラム
 public class CollisionTest : MonoBehaviour
 {
-	[SerializeField] GameObject targetObject;
-	[SerializeField] Rigidbody rigidbody;
-	[SerializeField] float power;
+	[SerializeField] GameObject targetObject;    // ターゲットにするオブジェクト
+
+	[SerializeField] Rigidbody rigidbody;	  // リジッドボディ
+
+	[SerializeField] float power;     // パワー
+    [SerializeField] float speed;     // スピード
+
+    [SerializeField] string moveXAxis;
+    [SerializeField] string moveZAxis;
 
 	void Update()
 	{
-		if(Input.GetMouseButtonDown(0))
-		{
-			transform.LookAt(targetObject.transform);
-			rigidbody.AddForce(transform.forward * power, ForceMode.Impulse);
-		}
+		Move();
+	}
+
+	void Move()
+	{
+		// X移動
+        float moveX = Input.GetAxis(moveXAxis) * speed;
+		// Z移動
+        float moveZ = Input.GetAxis(moveZAxis) * speed;
+		//Player移動
+		rigidbody.AddForce(transform.forward * moveZ, ForceMode.Force);
+        rigidbody.AddForce(transform.right * moveX, ForceMode.Force);
+
+        // クリックでターゲットに向かって飛んでいく
+        if (Input.GetMouseButtonDown(0))
+        {
+            transform.LookAt(targetObject.transform);
+            rigidbody.AddForce(transform.forward * power, ForceMode.Impulse);
+        }
 	}
 }
