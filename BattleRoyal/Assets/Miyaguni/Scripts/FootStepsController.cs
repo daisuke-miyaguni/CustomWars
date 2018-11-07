45c520cd90e1908fa993d117487326e033c44e23
miyaguni <c17018@std.it-college.ac.jp>
2018-11-07 10:43:35 +0900

    edit color

---
+++ BattleRoyal/Assets/Miyaguni/Scripts/FootStepsController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootStepsController : MonoBehaviour
+{
+    [SerializeField] int stepTime = 90;
+
+    void Start()
+    {
+        StartCoroutine(Disappearing());
+    }
+
+    IEnumerator Disappearing()
+    {
+        for (int i = 0; i < stepTime; i++)
+        {
+            GetComponent<MeshRenderer>().material.color = new Color(1, 1, 1, 1 - 1.0f * i / stepTime);
+            yield return null;
+        }
+        Destroy(gameObject);
+	}
+}
---
+++ BattleRoyal/Assets/Miyaguni/Scripts/FootStepsManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootStepsManager : MonoBehaviour
+{
+	[SerializeField] GameObject footSteps;
+	[SerializeField] GameObject player;
+
+	[SerializeField] string playerTag;
+    bool footStepsTrigger;
+
+	float time = 0;
+	float footStepsTime;
+
+	[SerializeField] float maxFootStepTime;
+
+    void Update()
+    {
+		if(footStepsTrigger)
+		{
+            InstantFootSteps();
+        }
+    }
+
+	void FootStepChecker()
+	{
+		if(footStepsTime > maxFootStepTime)
+		{
+			footStepsTrigger = false;
+		}
+	}
+
+	void InstantFootSteps()
+	{
+        time += Time.deltaTime;
+		footStepsTime += Time.deltaTime;
+        if (time > 0.35f)
+        {
+            time = 0;
+            Instantiate(footSteps, player.transform.position, player.transform.rotation);
+        }
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == playerTag)
+		{
+			player = other.gameObject;
+			footStepsTime = 0.0f;
+			footStepsTrigger = true;
+		}
+	}
+}
