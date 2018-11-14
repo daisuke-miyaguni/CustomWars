1294320b49d3d9cb742ca8ad0c4776b2225aeda0
c17018 <c17018@std.it-college.ac.jp>
2018-11-14 10:58:50 +0900

    add GlobeGimmick System

--- BattleRoyal/Assets/Miyaguni/Scripts/GlobeController.cs
+++ BattleRoyal/Assets/Miyaguni/Scripts/GlobeController.cs
@@ -18,19 +18,20 @@ public class GlobeController : MonoBehaviour
     {
     	if(other.gameObject.tag == "Player")
     	{
-    		other.transform.parent = gameObject.transform;
+    		// other.transform.parent = gameObject.transform;
     		// float rotateY = Input.GetAxis(rotateYAxis);
 
             float rotateY = Input.GetAxis(rotateYAxis) * rotateSpeed;
             rb.AddTorque(new Vector3(0, -rotateY, 0), ForceMode.Force);
+            // rb.AddTorque(new Vector3(-rotateY, 0, 0), ForceMode.Force);
         }
     }
 
-	void OnCollisionExit(Collision other)
-	{
-        if (other.gameObject.tag == "Player")
-        {
-            other.transform.parent = null;
-        }
-	}
+	// void OnCollisionExit(Collision other)
+	// {
+    //     if (other.gameObject.tag == "Player")
+    //     {
+    //         // other.transform.parent = null;
+    //     }
+	// }
 }
