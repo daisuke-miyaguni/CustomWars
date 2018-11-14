a522bc678c4cae57e38e399e5470f8a8060f452a
amuroo <c17001@std.it-college.ac.jp>
2018-11-14 12:15:10 +0900

    first commit

---
+++ BattleRoyal/Assets/Amuro/Scripts/DragSlot.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DragSlot : MonoBehaviour
+{
+
+    private ItemData dragItemData;
+
+    public void OnEnable()
+    {
+        gameObject.transform.localScale = new Vector3(1, 1, 1);
+    }
+
+    public void SetDragItem(ItemData dragItemData)                  //ドラッグ時のアイテム画像を取得
+    {
+        this.dragItemData = dragItemData;
+        transform.GetComponent<Image>().sprite = dragItemData.GetItemSprite();          //ProcessingSlot, CustomSlot, CreateSlotScript
+    }
+
+    public ItemData GetItem()
+    {
+        return dragItemData;
+    }
+
+    public void DeleteDragItem()
+    {
+        dragItemData = null;
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = Input.mousePosition;           //アイテム画像をうごかす
+	}
+}
---
+++ BattleRoyal/Assets/Amuro/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public static int atk　= 10;                  //プレイヤーの攻撃力
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(Vector3.forward);
+        }
+
+        else if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(Vector3.back);
+        }
+
+        else if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(Vector3.left);
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(Vector3.right);
+        }
+
+        else
+        {
+            rb.velocity = Vector3.zero;
+        }
+
+        // Debug.Log(atk);
+
+	}
+}
