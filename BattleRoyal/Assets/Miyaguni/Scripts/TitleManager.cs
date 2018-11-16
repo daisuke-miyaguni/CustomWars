6c6dae50dbb21eef6294981f376623ddc68fc10c
c17018 <c17018@std.it-college.ac.jp>
2018-11-16 14:31:08 +0900

    add TitleManager

---
+++ BattleRoyal/Assets/Miyaguni/Scripts/TitleManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleManager : MonoBehaviour
+{
+    SceneTransitioner sceneTransitioner;
+
+    void Start()
+    {
+        sceneTransitioner = gameObject.GetComponent<SceneTransitioner>();
+    }
+
+	void Update()
+	{
+		MoveLobby();
+	}
+
+	void MoveLobby()
+	{
+		if(Input.touchCount > 0 || Input.GetMouseButtonDown(0))
+		{
+			sceneTransitioner.MoveScene();
+		}
+	}
+}
