da36cfb132888e4fdc8a191c5a256a78e66e3435
c17018 <c17018@std.it-college.ac.jp>
2018-12-04 16:22:07 +0900

    Resolve conflicts

--- BattleRoyal/Assets/Amuro/Scripts/UI.cs
+++ BattleRoyal/Assets/Amuro/Scripts/UI.cs
@@ -31,10 +31,6 @@ public class UI : MonoBehaviour
 
     public void OnClick()
     {
-<<<<<<< HEAD
-=======
-
->>>>>>> fce8e1650e5b1c2a9406be04f2e403d5cfaa683f
         Pocket.SetActive(!Pocket.activeSelf);
         basePanel.SetActive(!basePanel.activeSelf);
     }
