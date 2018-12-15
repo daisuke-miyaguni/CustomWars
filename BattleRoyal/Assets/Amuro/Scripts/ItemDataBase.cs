71763f12e1bc36352e7c116e92009e825af0f789
c17018 <c17018@std.it-college.ac.jp>
2018-12-15 17:38:34 +0900

    アイテム番号とアイテム自体の設定のし直しとアイテムの説明と名前の編集

--- BattleRoyal/Assets/Amuro/Scripts/ItemDataBase.cs
+++ BattleRoyal/Assets/Amuro/Scripts/ItemDataBase.cs
@@ -8,12 +8,12 @@ public class ItemDataBase : MonoBehaviour
 
     void Awake()        //アイテムの作成(アイテム画像、アイテム名、アイテムタイプ、アイテム説明、アイテム攻撃力、アイテム防御力、使用アイテム判別)
     {
-        itemLists[0] = new ItemData(0, Resources.Load("kiyoneko", typeof(Sprite)) as Sprite, "キヨ猫", MyItemStatus.Item.parts1, "猫です。", 10, 0, PocketStatus.Pocket.none);
-        itemLists[1] = new ItemData(1, Resources.Load("poke_coin", typeof(Sprite)) as Sprite, "ポケコイン", MyItemStatus.Item.parts2, "コイン", 15, 0, PocketStatus.Pocket.none);
-        itemLists[2] = new ItemData(2, Resources.Load("wad", typeof(Sprite)) as Sprite, "ワドルディ", MyItemStatus.Item.parts3, "ワドワド", 20, 0, PocketStatus.Pocket.none);
+        itemLists[0] = new ItemData(0, Resources.Load("kiyoneko", typeof(Sprite)) as Sprite, "キャップ", MyItemStatus.Item.parts1, "攻撃小UP", 10, 0, PocketStatus.Pocket.none);
+        itemLists[1] = new ItemData(1, Resources.Load("poke_coin", typeof(Sprite)) as Sprite, "修正液", MyItemStatus.Item.parts2, "攻撃中UP", 15, 0, PocketStatus.Pocket.none);
+        itemLists[2] = new ItemData(2, Resources.Load("wad", typeof(Sprite)) as Sprite, "三角定規", MyItemStatus.Item.parts3, "攻撃大UP", 20, 0, PocketStatus.Pocket.none);
         itemLists[3] = new ItemData(3, Resources.Load("metamon", typeof(Sprite)) as Sprite, "メタモン", MyItemStatus.Item.mon, "変身", 0, 0, PocketStatus.Pocket.mon_p);
         itemLists[4] = new ItemData(4, Resources.Load("show", typeof(Sprite)) as Sprite, "ボール", MyItemStatus.Item.ball, "ポケボール", 0, 0, PocketStatus.Pocket.ball_p);
-        itemLists[5] = new ItemData(5, Resources.Load("riyo_s", typeof(Sprite)) as Sprite, "リヨ", MyItemStatus.Item.riyo, "やばい奴", 0, 0, PocketStatus.Pocket.riyo_p);
+        itemLists[5] = new ItemData(5, Resources.Load("riyo_s", typeof(Sprite)) as Sprite, "救急箱", MyItemStatus.Item.riyo, "回復", 0, 0, PocketStatus.Pocket.riyo_p);
 
     }
 
