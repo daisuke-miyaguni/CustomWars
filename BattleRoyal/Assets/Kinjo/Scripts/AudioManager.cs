d0196691c9158d38b0a37feff8ef4ce91a78e118
c17009Katsuki_Kinjo <c17009@std.it-college.ac.jp>
2018-12-30 05:11:53 +0900

    Create AudioManager and add to Resources

---
+++ BattleRoyal/Assets/Kinjo/Scripts/AudioManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+[SerializeField]private AudioSource source;
+[SerializeField]AudioClip titleBGM;
+[SerializeField]AudioClip mainBGM;
+private string beforeScene;
+	// Use this for initialization
+	void Start ()
+	{
+		source = GetComponent<AudioSource>();
+		beforeScene = SceneManager.GetActiveScene().name;
+		if(beforeScene == "Main")
+		{
+			source.clip = mainBGM;
+		} else {
+			source.clip = titleBGM;
+		}
+		source.Play();
+		SceneManager.activeSceneChanged += OnActiveSceneChanged;
+	}
+	
+	void OnActiveSceneChanged(Scene prevScene, Scene nextScene)
+	{
+		if (beforeScene == "Lobby")
+		{
+			print("aaa");
+			source.Stop();
+			source.clip = mainBGM;
+			source.Play();
+		} else if (beforeScene == "Main")
+		{
+			source.Stop();
+			source.clip = titleBGM;
+			source.Play();
+		}
+
+		beforeScene = nextScene.name;
+	}
+}
---
+++ BattleRoyal/Assets/Kinjo/Scripts/RuntimeInitializer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuntimeInitializer : MonoBehaviour {
+
+	[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+	private static void InitializeBeforeSceneLoad()
+	{
+		var manager = GameObject.Instantiate(Resources.Load("AudioManager"));
+		GameObject.DontDestroyOnLoad( manager );
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
