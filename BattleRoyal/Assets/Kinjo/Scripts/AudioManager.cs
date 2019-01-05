e97c67be7d53d3de401598160fd3d96d0d094581
c17009Katsuki_Kinjo <c17009@std.it-college.ac.jp>
2019-01-06 00:58:16 +0900

    Remake AudioManager and move to bgm,se files

--- BattleRoyal/Assets/Kinjo/Scripts/AudioManager.cs
+++ BattleRoyal/Assets/Kinjo/Scripts/AudioManager.cs
@@ -1,43 +1,111 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.SceneManagement;
-using UnityEngine;
 
-public class AudioManager : MonoBehaviour {
-[SerializeField]private AudioSource source;
-[SerializeField]AudioClip titleBGM;
-[SerializeField]AudioClip mainBGM;
-private string beforeScene;
-	// Use this for initialization
-	void Start ()
+///<summary>
+///BGMとSEを管理するシングルトン
+///</summary>
+public class AudioManager : SingletonMonoBehaviour<AudioManager>
+{
+	private string nextBGMName;
+	private string nextSEName;
+	private bool isFadeOut = false;
+	private float bgmFadeSpeedRate;
+
+	public AudioSource AttachBGMSource,AttachSESource;
+
+	private Dictionary<string,AudioClip>bgmDic,seDic;
+
+	private void Awake()
 	{
-		source = GetComponent<AudioSource>();
-		beforeScene = SceneManager.GetActiveScene().name;
-		if(beforeScene == "Main")
+		if(this != Instance)
 		{
-			source.clip = mainBGM;
-		} else {
-			source.clip = titleBGM;
+			Destroy(this);
+			return;
 		}
-		source.Play();
-		SceneManager.activeSceneChanged += OnActiveSceneChanged;
+
+		DontDestroyOnLoad(this.gameObject);
+
+		bgmDic = new Dictionary<string, AudioClip>();
+		seDic = new Dictionary<string, AudioClip>();
+
+		object[] bgmList = Resources.LoadAll("Audio/BGM");
+		object[] seList = Resources.LoadAll("Audio/SE");
+
+		foreach(AudioClip bgm in bgmList)
+		{
+			bgmDic[bgm.name] = bgm;
+		}
+		foreach(AudioClip se in seList)
+		{
+			seDic[se.name] = se;
+		}
+	}
+
+	///<summary>
+	///指定したファイルのSEを再生
+	///</summary>
+	public void PlaySE(string seName)
+	{
+		if(!seDic.ContainsKey(seName))
+		{
+			Debug.Log(seName + "という名前のファイルはありませぬ");
+			return;
+		}
+
+		nextSEName = seName;
+		AttachSESource.PlayOneShot(seDic[nextSEName] as AudioClip);
+	}
+
+	///<summary>
+	///指定したファイルのBGMを再生
+	///</summary>
+	public void PlayBGM(string bgmName,float fadeSpeed = 0.9f)
+	{
+		if(!bgmDic.ContainsKey(bgmName))
+		{
+			Debug.Log(bgmName + "という名前のファイルはありません");
+			return;
+		}
+
+		if (!AttachBGMSource.isPlaying) {
+			nextBGMName = "";
+			AttachBGMSource.clip = bgmDic [bgmName] as AudioClip;
+			AttachBGMSource.Play ();
+		}
+		//違うBGMが流れている時は、流れているBGMをフェードアウトさせてから次を流す。同じBGMが流れている時はスルー
+		else if (AttachBGMSource.clip.name != bgmName) {
+			nextBGMName = bgmName;
+			FadeOutBGM (fadeSpeed);
+		}
+	}
+
+	//BGMフェードアウト
+	public void FadeOutBGM(float fadeSpeed)
+	{
+		bgmFadeSpeedRate = fadeSpeed;
+		isFadeOut = true;
+	}
+
+	private void Update()
+	{
+		if(!isFadeOut)
+		{
+			return;
+		}
+
+		AttachBGMSource.volume -= Time.deltaTime * bgmFadeSpeedRate;
+		if(AttachBGMSource.volume <= 0)
+		{
+			AttachBGMSource.Stop();
+			AttachBGMSource.volume = PlayerPrefs.GetFloat("BGM_VOLUME_KEY",1.0f);
+			isFadeOut = false;
+
+			if(!string.IsNullOrEmpty(nextBGMName))
+			{
+				PlayBGM(nextBGMName);
+			}
+		}
 	}
 	
-	void OnActiveSceneChanged(Scene prevScene, Scene nextScene)
-	{
-		if (beforeScene == "Lobby")
-		{
-			print("aaa");
-			source.Stop();
-			source.clip = mainBGM;
-			source.Play();
-		} else if (beforeScene == "Main")
-		{
-			source.Stop();
-			source.clip = titleBGM;
-			source.Play();
-		}
-
-		beforeScene = nextScene.name;
-	}
 }
